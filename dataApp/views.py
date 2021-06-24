3b62f8b8b361d93121f6292a6340db321f576cb5
Arvind Shelke <shelkearvind3@ygmail.com>
2021-06-24 17:29:56 +0530

    initial

---
+++ dataApp/views.py
@@ -0,0 +1,25 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render, HttpResponse
+from django.urls.base import get_script_prefix
+import requests
+
+
+def index(request):  
+    result=requests.get('https://api.covid19api.com/summary')
+    countrySummary=result.json()['Countries']
+    india=[item for item in countrySummary if item["Country"] == "India"][0]
+    
+
+    result2=requests.get("https://api.covid19india.org/state_district_wise.json")
+    maha=result2.json()['Maharashtra']['districtData']
+    pune=maha["Pune"]
+    Thane=maha["Thane"]
+    Mumbai=maha["Mumbai"]
+    Nashik=maha["Nashik"]
+    Nagpur=maha["Nagpur"]
+
+    result=requests.get('https://api.covid19india.org/data.json')
+    stateSummary=result.json()['statewise']
+    mh=[item for item in stateSummary if item["state"] == "Maharashtra"][0]
+    
+    return render(request, 'dataApp/index.html', {'maha': maha, "india":india, 'mh':mh, 'pune':pune, 'Thane':Thane, 'Mumbai':Mumbai, 'Nashik':Nashik, 'Nagpur':Nagpur})
