1b4e4214406ffb91d07adfd94f7725566eb58af0
Arvind Shelke <ucdonor5@gmail.com>
2021-08-15 10:23:02 +0530

    view and route

--- dataApp/urls.py
+++ dataApp/urls.py
@@ -1,8 +1,11 @@
 
 from .views import index
 from django.urls import path
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', index)
 ]
- 
+
+urlpatterns+=staticfiles_urlpatterns()
